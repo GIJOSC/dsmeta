8fdde5b1cdadd9c3d3898a6904c5d38864cb424f
Gesiel Neves <neves.gesiel@gmail.com>
2022-07-18 01:20:33 -0300

    Date Select

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,9 +1,13 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.reposiories.SaleRepository;
@@ -14,18 +18,14 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 
-	public List<Sale> findSales() {
-		return repository.findAll();
+	@GetMapping
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate min = LocalDate.parse(minDate);
+		LocalDate max = LocalDate.parse(maxDate);
+		
+		return repository.findAll(pageable);
 
-	}
-
-	public List<Sale> getRepository() {
-		return null;
-
-	}
-
-	public void setRepository(SaleRepository repository) {
-		this.repository = repository;
 	}
 
 }
